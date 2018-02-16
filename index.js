db6a8cca2227345943a698c78ff733ca5a27bd12
Gyokan <gyokan.nezhdetov@gmail.com>
2018-02-16 20:31:24 +0200

    Added add product page

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const http = require('http')
-const port = 8000
+const handlers = require('./handlers')
+
+const port = 3500
 
 http.createServer((req, res) => {
-  res.writeHead(200, {
-    'Content-Type': 'text/plain'
-  })
-
-  res.write('Hello!')
-  res.end()
+  for (let handler of handlers) {
+    if (!handler(req, res)) {
+      break
+    }
+  }
 }).listen(port)
+
+console.log(`Node.js server running on port ${port}`)
