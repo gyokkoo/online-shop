bdc6ffd1259ab3942a8048b61dd7ae289ba64a54
Gyokan <gyokan.nezhdetov@gmail.com>
2018-02-21 23:04:55 +0200

    Added more functionality

--- controllers/category.js
+++ controllers/category.js
@@ -6,7 +6,24 @@ module.exports.addGet = (req, res) => {
 
 module.exports.addPost = (req, res) => {
   let category = req.body
+  category.creator = req.user._id
+
   Category.create(category).then(() => {
     res.redirect('/')
   })
+}
+
+module.exports.productByCategory = (req, res) => {
+  let categoryName = req.params.category
+
+  Category.findOne({name: categoryName})
+    .populate('products')
+    .then((category) => {
+      if (!category) {
+        res.sendStatus(404)
+        return
+      }
+
+      res.render('category/products', {category: category})
+    })
 }
--- controllers/home.js
+++ controllers/home.js
@@ -3,12 +3,17 @@ const Product = require('../models/Product')
 module.exports.index = (req, res) => {
   let queryData = req.query
 
-  Product.find().populate('category').then((products) => {
+  Product.find({buyer: null}).populate('category').then((products) => {
     if (queryData.query) {
-      products = products.filter(
-        p => p.name.toLowerCase().includes(queryData.query))
+      products = products.filter(p => p.name.toLowerCase().includes(queryData.query))
     }
 
-    res.render('home/index', { products: products })
+    let data = {products: products}
+    if (req.query.error) {
+      data.error = req.query.error
+    } else if (req.query.success) {
+      data.success = req.query.success
+    }
+    res.render('home/index', data)
   })
 }
