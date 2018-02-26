8055329d670c56d63419b980bff14802a90cb2df
Gyokan <gyokan.nezhdetov@gmail.com>
2018-02-26 09:42:24 +0200

    Fixed css not found bug

--- index.js
+++ index.js
@@ -4,13 +4,12 @@ const database = require('./config/database.config')
 const express = require('express')
 
 let app = express()
-let environment = process.env.NODE_ENV || 'development'
+let environment = process.env.NODE_environment || 'development'
 
 database(config[environment])
-app.listen(port)
-
 require('./config/express')(app, config[environment])
 require('./config/routes')(app)
 require('./config/passport')()
 
+app.listen(port)
 console.log(`Node.js server running on port ${port}`)
